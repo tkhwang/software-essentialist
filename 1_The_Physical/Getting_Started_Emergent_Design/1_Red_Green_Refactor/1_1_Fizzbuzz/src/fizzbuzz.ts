2ce3003847eaa82e629d17d10e02196fcb4be7dd
tkhwang <tkhwang@gmail.com>
2025-10-14 20:35:08 +0900

    fix(fizzbuzz): Update error handling for input validation
    - Modified the fizzbuzz function to throw an error if the input number is less than 1 or greater than 100, ensuring valid input range.

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export const fizzbuzz = (number: number): string => {
-    if (number > 100) {
-        throw new Error("Number must be less than 100");
+    if (number < 1 || number > 100) {
+        throw new Error("Number must be between 1 and 100");
     }
 
     const isDivisibleBy3 = number % 3 === 0;
