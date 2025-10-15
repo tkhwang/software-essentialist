c8099925657cd642be702538bf58b58bdec1464f
tkhwang <tkhwang@gmail.com>
2025-10-15 15:46:23 +0900

    refactor(stats-calculator): optimize sequence length handling in calculation
    - Simplified the handling of the sequence length by directly assigning it at the beginning of the calculation method.
    - Updated the logic to return default statistics for empty sequences without unnecessary variable initialization.

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/stats-calculator.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/stats-calculator.ts
@@ -8,20 +8,14 @@ export type StatsCalculatorResult = {
 
 export class StatsCalculator {
     static calculate(sequence: number[]): StatsCalculatorResult {
+        const numberOfSequence = sequence.length;
+
         let min = Infinity;
         let max = -Infinity;
         let sum = 0;
-        let numberOfSequence = 0;
         let average = 0;
 
-        if (sequence.length === 0) {
-            return {
-                min,
-                max,
-                numberOfSequence,
-                average,
-            };
-        }
+        if (numberOfSequence === 0) return { min, max, numberOfSequence, average };
 
         for (const value of sequence) {
             if (value < min) min = value;
@@ -29,7 +23,6 @@ export class StatsCalculator {
             sum += value;
         }
 
-        numberOfSequence = sequence.length;
         average = Number((sum / numberOfSequence).toFixed(12));
 
         return {
