6e277d61eb3e4c09dcaa853b910df3d2b0316b3d
tkhwang <tkhwang@gmail.com>
2025-10-15 17:16:28 +0900

    refactor(military-time-validator): simplify time validation logic
    - Streamlined the validation process in the MilitaryTimeValidator class by consolidating time parsing and checks.
    - Replaced multiple helper functions with direct array destructuring for improved readability.
    - Enhanced the validation logic to ensure correct time range format with fewer lines of code.

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/military-time-validator.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/military-time-validator.ts
@@ -1,38 +1,19 @@
-
 export class MilitaryTimeValidator {
-    private static readonly RANGE_SEPARATOR = /\s*-\s*/;
     private static readonly TIME_REGEX = /^([01]\d|2[0-3]):([0-5]\d)$/;
 
-
     static validate(timeText: string): boolean {
-        const getStartTime = (text: string) => text.split(' - ')[0];
-        const getEndTime = (text: string) => text.split(' - ')[1];
-        const getHour = (text: string) => text.split(":")[0];
-        const getMinute = (text: string) => text.split(":")[1];
+        const parts = timeText.split(' - ');
+        if (parts.length !== 2) return false;
 
-        // check given timeText is in correct format
-        if (timeText === "") return false;
-        if (timeText.indexOf(" - ") === -1) return false;
-        if (getStartTime(timeText).indexOf(':') === -1) return false;
-        if (getEndTime(timeText).indexOf(':') === -1) return false;
-
-        // check time is in correct format
-        const startTime = getStartTime(timeText);
-        const endTime = getEndTime(timeText);
+        const [startTime, endTime] = parts;
         if (!this.isValidTime(startTime) || !this.isValidTime(endTime)) return false;
 
-        const startTimeHour = getHour(startTime);
-        const startTimeMinute = getMinute(startTime);
-        const endTimeHour = getHour(endTime);
-        const endTimeMinute = getMinute(endTime);
+        const [startHour, startMinute] = startTime.split(':').map(Number);
+        const [endHour, endMinute] = endTime.split(':').map(Number);
 
-        if (startTimeHour === endTimeHour) {
-            return startTimeMinute < endTimeMinute;
-        } else {
-            return startTimeHour < endTimeHour;
-        }
+        if (startHour === endHour) return startMinute < endMinute;
 
-        return true;
+        return startHour < endHour;
     }
 
     private static isValidTime(time: string): boolean {
