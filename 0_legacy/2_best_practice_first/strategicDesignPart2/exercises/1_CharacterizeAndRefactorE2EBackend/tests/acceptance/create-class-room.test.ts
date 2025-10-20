1ac2d21e6361b5d59c7f8024dc24fb675c7788fa
tkhwang <tkhwang@gmail.com>
2025-10-20 14:29:00 +0900

    feat(tests): add acceptance tests for class room creation
    - Introduced new acceptance tests for creating class rooms using Jest and Cucumber.
    - Implemented scenarios for successful and failed class room creation, ensuring proper validation and response handling.
    - Added corresponding feature file to define the behavior of class room creation.

---
+++ 0_legacy/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/tests/acceptance/create-class-room.test.ts
@@ -0,0 +1,52 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import path from 'path';
+import request from 'supertest';
+import { app } from '../../src';
+import { resetDatabase } from '../fixtures/reset';
+
+const feature = loadFeature(path.join(__dirname, '../features/create-class-room.feature'));
+
+defineFeature(feature, (test) => {
+    afterEach(async () => {
+        await resetDatabase();
+    });
+
+    test("Sucessfully create a class room", ({ given, when, then }) => {
+        let requestBody: any = {};
+        let response: any = {};
+
+        given(/^I want to create a class room named "(.*)"$/, (name) => {
+            requestBody = {
+                name,
+            };
+        });
+
+        when("I request to create a class room", async () => {
+            response = await request(app).post("/classes").send(requestBody);
+        });
+
+        then("the class room should be successfully created", () => {
+            expect(response.status).toBe(201);
+            expect(response.body.data.name).toBe(requestBody.name);
+        });
+    });
+
+    test("Fail to create a class room", ({ given, when, then }) => {
+        let requestBody: any = {};
+        let response: any = {};
+
+        given("I want to create a class room with no name", () => {
+            requestBody = {};
+        });
+
+        when("I send a request to create a class room", async () => {
+            response = await request(app).post("/classes").send(requestBody);
+        });
+
+        then("the class room should not be created", () => {
+            expect(response.status).toBe(400);
+            expect(response.body.success).toBeFalsy();
+            expect(response.body.error).toBe("ValidationError");
+        });
+    });
+});
